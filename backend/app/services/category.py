409a72aec480b710735a12fbaafe03c0b2e5726b
Jeremy <jeremyngunawan@gmail.com>
2025-12-04 18:13:01 +0700

    kiodsgnodg

--- backend/app/services/category.py
+++ backend/app/services/category.py
@@ -19,3 +19,10 @@ def insert_categories(name,bg='#FFFFFF',text='#000000'):
         conn.close()
     except Exception as e:
         return {"status": "error", "message": str(e)}
+
+if __name__ == '__main__':
+    # py -m uv run -m app.services.category
+    '''
+    test here
+    '''
+    print(get_categories())
